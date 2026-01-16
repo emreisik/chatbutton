2117d3d0b7e8d7ee495ca6037a3ae13e05e32d7d
Emre <emreisik20@gmail.com>
2026-01-16 15:26:09 +0300

    Add frontend build - Admin panel ready

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
+      input: resolve(__dirname, "index.html"),
     },
   },
   server: {
